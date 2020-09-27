cb0db73e46b9241ba98d11389b86eabfe59ba74d
Andre Doumad <andre@stringkeeper.com>
2020-09-27 00:48:54 -0700

    updated fetching and rendering of posts.

--- blogs/client/src/App.js
+++ blogs/client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
+
 export default () => {
     return (
     <div className="container">
         <h1>Blog App</h1>
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
     </div>
     );
 }
