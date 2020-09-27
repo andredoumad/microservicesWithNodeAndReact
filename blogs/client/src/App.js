9b5ccd1ece229340634e62ed8f4497c0c9dd9558
Andre Doumad <andre@stringkeeper.com>
2020-09-27 00:21:46 -0700

    added bootstrap, post button, cors

--- blogs/client/src/App.js
+++ blogs/client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PostCreate from './PostCreate';
 export default () => {
     return (
-    <div>
+    <div className="container">
         <h1>Blog App</h1>
         <PostCreate />
     </div>
